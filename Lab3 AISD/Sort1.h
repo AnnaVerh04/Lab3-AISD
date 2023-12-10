64ab3f1e1707258209714bdc8de945222b193d73
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-12-10 12:23:46 +0400

    final changes

--- Lab3 AISD/Sort1.h
+++ Lab3 AISD/Sort1.h
@@ -8,14 +8,6 @@ struct stats {
     size_t copy_count = 0;
 };
 
-int random(int x, int y) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dist(x, y);
-    return dist(gen);
-}
-
-
 
 stats selectionSort(std::vector<int>& data) {
 
@@ -34,7 +26,7 @@ stats selectionSort(std::vector<int>& data) {
         }
 
         std::swap(data[i], data[minIndex]);
-        sortingState.copy_count+=2;
+        ++sortingState.copy_count;
     }
 
     return sortingState;
@@ -60,7 +52,7 @@ stats selectionSort(Iterator begin, Iterator end) {
         }
 
         std::iter_swap(i, minIndex);
-        sortingState.copy_count+=2;
+        ++sortingState.copy_count;
     }
 
     return sortingState;
@@ -70,16 +62,16 @@ stats selectionSort(Iterator begin, Iterator end) {
 stats shellSort(std::vector<int>& data) {
     stats sortingState;
 
-    for (int s = data.size() / 2; s > 0; s /= 2)    //вычисляем шаг
+    for (int s = data.size() / 2; s > 0; s /= 2)    
     {
-        for (int i = s; i < data.size(); ++i) { //  рассмотрение всех элементов с каким-то шагом
-            for (int j = i - s; j >= 0; j -= s)   // шагаем
+        for (int i = s; i < data.size(); ++i) { 
+            for (int j = i - s; j >= 0; j -= s)   
             {
                 ++sortingState.comparison_count;
                 if (data[j] > data[j + s])
                 {
                     std::swap(data[j], data[j + s]);
-                    sortingState.copy_count += 2;
+                    ++sortingState.copy_count;
                 }
             }
         }
@@ -99,7 +91,7 @@ stats shellSort(Iterator begin, Iterator end) {
             for (auto j = i; j >= begin + s && *j < *(j - s); j -= s) {
                 ++sorting_stats.comparison_count;
                 std::iter_swap(j, j - s);
-                sorting_stats.copy_count += 2;
+                ++sorting_stats.copy_count;
             }
         }
     }
@@ -140,7 +132,7 @@ stats heapSort(std::vector<int>& data) {
 
     int n = data.size();
 
-    for (int i = n / 2 - 1; i >= 0; --i)  // формируем max-heap из обычного массива
+    for (int i = n / 2 - 1; i >= 0; --i)  
     {
         heapfy(data, n, i, sortingState);
     }
@@ -148,7 +140,7 @@ stats heapSort(std::vector<int>& data) {
     for (int i = n - 1; i > 0; --i)
     {
         std::swap(data[0], data[i]);
-        sortingState.copy_count += 2;
+        ++sortingState.copy_count;
         heapfy(data, i, 0, sortingState);
     }
 
@@ -194,9 +186,66 @@ stats heapSort(Iterator begin, Iterator end) {
 
     for (auto i = n - 1; i > 0; --i) {
         std::swap(*begin, *(begin + i));
-        sortingState.copy_count += 2;
+        ++sortingState.copy_count;
         heapify(begin, end, i, 0, sortingState);
     }
+
+    return sortingState;
+}
+
+int random(int x, int y) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(x, y);
+    return dist(gen);
+}
+
+std::vector<int> generateRandomArray(size_t size) {
+    std::vector<int> res;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        res.push_back(random(0, 100));
+    }
+    return res;
+}
+
+
+std::vector<int> generateSortArray(size_t size) {
+    std::vector<int> res;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        res.push_back(i);
+    }
+    return res;
+}
+
+
+std::vector<int> generateReverseSortArray(size_t size) {
+    std::vector<int> res;
+
+    for (size_t i = size; i > 0; --i)
+    {
+        res.push_back(i);
+    }
+    return res;
+}
+
+stats runExperiment(std::vector<int>& data, int scenario, stats(*pointerToSort)(std::vector<int>& data)) {
+    stats sortingState;
+
+    if (scenario == 1) {
+        data = generateRandomArray(data.size());
+    }
+    else if (scenario == 2) {
+        data = generateSortArray(data.size());
+    }
+    else if (scenario == 3) {
+        data = generateReverseSortArray(data.size());
+    }
+
+    sortingState = pointerToSort(data);
 
     return sortingState;
 }
--- Lab3 AISD/Sort3.cpp
+++ Lab3 AISD/Sort3.cpp
@@ -1,25 +1,64 @@
+#include "Sort1.h"
 #include <iostream>
 #include <vector>
 #include <string>
-#include "Sort1.h"
+#include <random>
+
+
+
+
 
 int main()
 {
-    stats sortingState;
+    std::vector<int> data;
+    std::vector<size_t> vecSize = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+    std::vector<int> scenarios = { 1,2,3 };
 
-
-    std::vector<std::string> vec = { "a", "d", "w", "h", "e"};
-    sortingState = shellSort(vec.begin(), vec.end());
-
-
-    for (size_t i = 0; i < vec.size(); i++)
+    for (auto scenario : scenarios)
     {
-        std::cout << vec[i] << " ";
+        switch (scenario)
+        {
+        case 1:
+            for (auto size : vecSize)
+            {
+                data.resize(size);
+                stats avgStats;
+
+                for (size_t i = 0; i < 100; i++)
+                {
+                    stats experimentStats = runExperiment(data, scenario, selectionSort);
+
+                    avgStats.comparison_count += experimentStats.comparison_count;
+                    avgStats.copy_count += experimentStats.copy_count;
+                }
+
+                avgStats.comparison_count /= 100;
+                avgStats.copy_count /= 100;
+
+                std::cout << "Vec size: " << size << ", Average comparison_count: " << avgStats.comparison_count << ", Average copy_count: " << avgStats.copy_count << std::endl;
+            }
+            break;
+
+        case 2:
+            data.clear();
+            for (auto size : vecSize)
+            {
+                data.resize(size);
+                stats experimentStats = runExperiment(data, scenario, selectionSort);
+                std::cout << "Vec size: " << size << ", comparison_count:" << experimentStats.comparison_count << ", copy_count:" << experimentStats.copy_count << std::endl;
+            }
+            break;
+
+        case 3:
+            data.clear();
+            for (auto size : vecSize)
+            {
+                data.resize(size);
+                stats experimentStats = runExperiment(data, scenario, selectionSort);
+                std::cout << "Vec size: " << size << ", comparison_count:" << experimentStats.comparison_count << ", copy_count:" << experimentStats.copy_count << std::endl;
+            }
+            break;
+
+        }
     }
-    std::cout << "\n";
-
-    std::cout << sortingState.comparison_count << "\n";
-    std::cout << sortingState.copy_count << "\n";
-
-
 }
